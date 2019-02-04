fc889f1d49382d94e96d44a537dab1bd06cf6a35
jigarshah8055 <jigarshah2097@gmail.com>
2019-02-04 19:07:23 +0530

    CR acronym with changes

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -1,12 +1,11 @@
 # used for acronyms
 class Acronym
-  attr_accessor :word, :acronym
-  def self.abbreviate(str)
-    @word = str.split(/\W+/)
-    @acronym = ''
-    @word.each do |single|
-      @acronym.concat(single[0].upcase)
+  def self.abbreviate(sy
+    word = str.split(/\W+/)
+    acronym = ''
+    word.each do |single|
+    acronym.concat(single.first.upcase)
     end
-    p @acronym
+    acronym
   end
 end
